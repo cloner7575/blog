0b5dafe9a4d8be21b4d9dd0c9370ace0cc9d9c4d
mrzjk <mrzjkb1375@gmail.com>
2024-11-19 11:41:21 +0330

    all posts pages added

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
 
-from .views import IndexView, PostDetailView
+from .views import IndexView, PostDetailView, AllPostsView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     re_path(r'^(?P<slug>[\w-]+)/$', PostDetailView.as_view(), name='post_detail'),
+    path('posts/', AllPostsView.as_view(), name='all_posts'),
 ]
