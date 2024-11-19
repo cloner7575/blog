63c3154e17161254d5550e47807592eba53e2460
mrzjk <mrzjkb1375@gmail.com>
2024-11-19 10:57:49 +0330

    static deleted

--- core/settings.py
+++ core/settings.py
@@ -140,12 +140,11 @@ AUTH_USER_MODEL = 'accounts.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/public/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "public","static")
+MEDIA_URL = '/public/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "public","media")
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
